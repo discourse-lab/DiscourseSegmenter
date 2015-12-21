688abae5467c69345d47cf6c94ceace11ef2d04c
Wladimir Sidorenko <wlsidorenko@gmail.com>
2015-12-22 00:42:16 +0100

    added cross-validation for BitPar segmenter

--- dsegmenter/bparseg/bparsegmenter.py
+++ dsegmenter/bparseg/bparsegmenter.py
@@ -3,8 +3,7 @@
 
 ##################################################################
 # Documentation
-"""
-Module providing discourse segmenter for constituency trees.
+"""Module providing discourse segmenter for constituency trees.
 
 Constants:
 SUBSTITUTEF - custom weighting function used for token alignment
@@ -62,36 +61,36 @@ ESCAPE_SLASH_RE = re.compile(r"\\/")
 
 ##################################################################
 # Methods
-locale.setlocale(locale.LC_ALL, '')
+locale.setlocale(locale.LC_ALL, "")
 
 def _ispunct(a_word):
-    """
-    Check if word consists only of punctuation characters
+    """Check if word consists only of punctuation characters.
 
     @param a_word - word to check
 
     @return True if word consists only of punctuation characters, False otherwise
+
     """
     return all(c in string.punctuation for c in a_word)
 
 def _prune_punc(a_toks):
-    """
-    Remove tokens representing punctuation from set
+    """Remove tokens representing punctuation from set.
 
     @param a_toks - tokens to prune
 
     @return token set with punctuation tokens removed
+
     """
     return frozenset([tok for tok in a_toks if not _ispunct(tok[-1])])
 
 def _translate_toks(a_toks, a_translation):
-    """
-    Translate tokens and return translated set
+    """Translate tokens and return translated set.
 
     @param a_toks - tokens to be translated
     @param a_translation - translation dictionary for tokens
 
     @return translated set of tokens
+
     """
     if a_translation is None:
         return a_toks
@@ -102,13 +101,13 @@ def _translate_toks(a_toks, a_translation):
     return frozenset(ret)
 
 def tree2tok(a_tree, a_start = 0):
-    """
-    Create dictionary mapping constituency trees to numbered tokens
+    """Create dictionary mapping constituency trees to numbered tokens.
 
     @param a_tree - tree to analyze
     @param a_start - starting position of the first token
 
     @return dictionary mapping subtrees to their yields
+
     """
     rset = set()
     chset = None
@@ -127,12 +126,12 @@ def tree2tok(a_tree, a_start = 0):
     return tr2tk
 
 def read_trees(a_lines, a_one_per_line = False):
-    """
-    Read file and return a list of constituent dictionaries
+    """Read file and return a list of constituent dictionaries.
 
     @param a_lines - decoded lines of the input file
 
     @return list of dictionaries mapping tokens to trees and a list of trees
+
     """
     ctrees = CTree.parse_lines(a_lines, a_one_per_line = a_one_per_line)
     # generate dictionaries mapping trees' yields to trees
@@ -154,12 +153,12 @@ def read_trees(a_lines, a_one_per_line = False):
     return toks2trees, ctrees
 
 def read_segments(a_lines):
-    """
-    Read file and return a list of segment dictionaries
+    """Read file and return a list of segment dictionaries.
 
     @param a_lines - decoded lines of the input file
 
     @return dictionary which maps tokens to segments
+
     """
     segs2toks = {}
     s_c = t_c = 0
@@ -217,13 +216,13 @@ def read_segments(a_lines):
     return toks2segs
 
 def trees2segs(a_toks2trees, a_toks2segs):
-    """
-    Align trees with corresponding segments
+    """Align trees with corresponding segments.
 
     @param a_toks2trees - dictionary mapping tokens to trees
     @param a_toks2segs - dictionary mapping tokens to segments
 
     @return tree-segment dictionary
+
     """
     # prune empty trees and their corresponding segments
     tree2seg = {t: None for val in a_toks2trees.values() for t in val}
@@ -269,12 +268,12 @@ def trees2segs(a_toks2trees, a_toks2segs):
     return tree2seg
 
 def featgen(a_tree):
-    """
-    Generate features for the given BitPar tree.
+    """Generate features for the given BitPar tree.
 
     @param a_tree - BitPar tree for which we should generate features
 
     @return list of string features
+
     """
     assert a_tree.leaves(), "Tree does not contain leaves."
     # add unigram features
@@ -311,8 +310,7 @@ def featgen(a_tree):
     return ret
 
 def classify(a_classifier, a_featgen, a_el, a_default = None):
-    """
-    Classify given element.
+    """Classify given element.
 
     @param a_classifier - model which should make predictions
     @param a_featgen - feature generation function
@@ -321,6 +319,7 @@ def classify(a_classifier, a_featgen, a_el, a_default = None):
                        not yield segment
 
     @return assigned class
+
     """
     prediction = a_classifier.predict(a_featgen(a_el))[0]
     return a_default if prediction is None or prediction == NONE else prediction
@@ -328,8 +327,7 @@ def classify(a_classifier, a_featgen, a_el, a_default = None):
 ##################################################################
 # Class
 class BparSegmenter(object):
-    """
-    Class for perfoming discourse segmentation on constituency trees.
+    """Class for perfoming discourse segmentation on constituency trees.
 
     Constants:
     DEFAULT_CLASSIFIER - default classification method
@@ -359,26 +357,26 @@ class BparSegmenter(object):
 
     def __init__(self, a_featgen = featgen, a_classify = classify, \
                      a_model = DEFAULT_MODEL):
-        """
-        Class constructor
+        """Class constructor.
 
         @param a_featgen - function to be used for feature generation
         @param a_classify - pointer to 2-arg function which predicts segment class for BitPar
                             tree based on the model and features generated for that tree
         @param a_model - path to a pre-trained model (previously dumped by joblib) or
                          valid classification object or None
+
         """
         self.featgen = a_featgen
         self.classify = a_classify
         self._update_segmenter(a_model)
 
     def segment(self, a_trees):
-        """
-        Create discourse segments based on the BitPar trees
+        """Create discourse segments based on the BitPar trees.
 
         @param a_trees - list of sentence trees to be parsed
 
         @return iterator over constructed segment trees
+
         """
         seg_idx = 0
         segments = []
@@ -398,95 +396,15 @@ class BparSegmenter(object):
             seg_idx = len(segments)
         return segments
 
-    # def cv_train(self, a_fname2trees, a_fname2segs, a_path, a_model = DEFAULT_PIPELINE, \
-    #                  a_folds = N_FOLDS, a_out = False, a_out_dir = os.getcwd, a_out_sfx = ""):
-    #     """
-    #     Train segmenter model in cross vaidation mode and store it
-
-    #     @param a_fname2trees - dictionary mapping file names to sentence trees
-    #     @param a_fname2segs - dictionary mapping file names to segments
-    #     @param a_out - directory for writing output files
-    #     @param a_out_dir - directory for writing output files
-    #     @param a_out_sfx - suffix which should be appended to output files
-
-    #     @return \c 3-tuple containing list of macro F-scores, micro
-    #     F-scores, number of best fold
-    #     """
-    #     assert len(a_fname2trees) == len(a_fname2segs), \
-    #         "Unmatching number of files with trees and segments."
-    #     # estimate number of folds
-    #     a_folds = min(len(a_fname2trees), a_folds)
-    #     folds = KFold(len(a_fname2trees), a_folds)
-    #     # generate features for trees
-    #     fname2feats = {fname: [self.featgen(t) for t in trees] \
-    #                        for fname, trees in a_fname2trees.iteritems()}
-
-    #     best_i = -1
-    #     pred_segs = []
-    #     pipeline = None
-    #     test_fname = ""
-    #     out_fname = ""
-    #     out_fnames = []
-    #     istart = ilen = 0
-    #     trees = []
-    #     fname2range = {}
-    #     processed_fnames = {}
-    #     best_macro_f1 = float("-inf")
-    #     macro_f1 = 0; macro_F1s = []
-    #     micro_f1 = 0; micro_F1s = []
-    #     train_segs = test_segs = None
-    #     train_feats = test_feats = None
-    #     for i, (train, test) in enumerate(folds):
-    #         print >> sys.stderr, "Fold: {:d}".format(i)
-    #         train_feats = [feat for k in train for feat in fname2feats[fnames[k]]]
-    #         train_segs = [str(seg) for k in train for seg in a_fname2segs[fnames[k]]]
-    #         istart = 0
-    #         for k in test:
-    #             ilen = len(fname2featseg[fnames[k]])
-    #             fname2range[fnames[k]] = [istart, istart + ilen]
-    #             istart += ilen
-    #         test_feats = [feat for k in test for feat in fname2feats[fnames[k]]]
-    #         test_segs = [str(seg) for k in test for seg in a_fname2segs[fnames[k]]]
-    #         # train classifier
-    #         a_model.fit(train_feats, train_segs)
-    #         # obtain new predictions
-    #         pred_segs = a_model.predict(test_feats)
-    #         # update F1 scores
-    #         _, _, macro_f1, _ = precision_recall_fscore_support(test_segs, pred_segs, average = 'macro', \
-    #                                                                 class_weight = 'auto')
-    #         _, _, micro_f1, _ = precision_recall_fscore_support(test_segs, pred_segs, average = 'micro', \
-    #                                                                 class_weight = 'auto')
-    #         macro_F1s.append(macro_f1); micro_F1s.append(micro_f1)
-    #         print >> sys.stderr, "Macro F1: {:.2%}".format(macro_f1)
-    #         print >> sys.stderr, "Micro F1: {:.2%}".format(micro_f1)
-    #         # update maximum macro F-score and store the most successful model
-    #         if macro_f1 > best_macro_f1:
-    #             best_i = i
-    #             best_macro_f1 = macro_f1
-    #             joblib.dump(a_model, a_path)
-    #         # generate new output files, if necessary
-    #         if a_output:
-    #             for k in test:
-    #                 test_fname = fnames[k]
-    #                 if test_fname in processed_fnames and processed_fnames[test_fname] > macro_f1:
-    #                     continue
-    #                 processed_fnames[test_fname] = macro_f1
-    #                 trees.append(a_fname2trees[test_fname])
-    #                 out_fname = os.path.join(a_out_dir, os.path.splitext(test_fname)[0] + a_out_sfx)
-    #                 out_fnames.append(out_fname)
-    #             bpar_segmenter_segment(pipeline, featgen, trees, out_fnames)
-    #             del trees[:]; del out_fnames[:]; fname2range.clear()
-    #     return (macro_F1s, micro_F1s, best_i)
-
     def train(self, a_trees, a_segs, a_path):
-        """
-        Train segmenter model
+        """Train segmenter model.
 
         @param a_trees - list of BitPar trees
         @param a_segs - list of discourse segments
         @param a_path - path to file in which the trained model should be stored
 
         @return \c void
+
         """
         # drop current model
         self._update_segmenter(self.DEFAULT_PIPELINE)
@@ -499,13 +417,13 @@ class BparSegmenter(object):
         joblib.dump(self.model, a_path)
 
     def test(self, a_trees, a_segments):
-        """
-        Estimate performance of segmenter model
+        """Estimate performance of segmenter model.
 
         @param a_trees - list of BitPar trees
         @param a_segments - list of corresponding gold segments for trees
 
         @return 2-tuple with macro and micr F-score
+
         """
         if self.model is None:
             return (0, 0)
@@ -518,26 +436,26 @@ class BparSegmenter(object):
         return (macro_f1, micro_f1)
 
     def _train(self, a_feats, a_segs, a_model):
-        """
-        Train segmenter model
+        """Train segmenter model.
 
         @param a_feats - list of BitPar featuress
         @param a_segs - list of discourse segments
         @param a_model - model object whose parameters should be fit
 
         @return \c void
+
         """
         # train classifier
         a_model.fit(a_feats, a_segs)
         self._update_segmenter(a_model)
 
     def _update_segmenter(self, a_model):
-        """
-        Update model, decision function, and internal segmenter
+        """Update model, decision function, and internal segmenter.
 
         @param a_model - model used by classifier
 
         @return \c void
+
         """
         if a_model is None:
             self.model = a_model
