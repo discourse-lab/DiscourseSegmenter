748eaf94d1fdc8e5791d359d394910587a39fee5
Wladimir Sidorenko <wlsidorenko@gmail.com>
2017-06-30 12:40:14 +0200

    project version set to 0.2.0

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ with codecs.open(path.join(PWD, "test-requirements.txt"),
 # setup()
 setup(
     name="dsegmenter",
-    version="0.2.12",
+    version="0.2.0",
     description=("Collection of discourse segmenters "
                  "(with pre-trained models for German)"),
     long_description=long_description,
